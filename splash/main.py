aa28861e666d66398ab73459a4280614ac4e5e23
mwae-bu <mwae@bu.edu>
2024-08-19 20:24:17 -0400

    ModalView -> Popup

--- splash/main.py
+++ splash/main.py
@@ -2,7 +2,12 @@
     # Code by Mike Waetzman (mwae@bu.edu), Anna LaPrade (alaprade@bu.edu)
     # UI/UX Design by Anna LaPrade (alaprade@bu.edu)
 
+# Notes for Members:
+    # Check you're using Python 3.2 (minimum) and that you've got pip
+    # Have you installed Kivy and OpenCV?
+
 # BUTM Code: PS-CRY-002a1
+import cv2
 
 import kivy
 from kivy.app import App
@@ -15,26 +20,27 @@ from kivy.uix.textinput import TextInput
 from kivy.config import Config              # May not be needed later
 from kivy.core.window import Window
 from kivy.clock import Clock
-
 from kivy.uix.modalview import ModalView    # Like the Popup widget
-
+from kivy.uix.popup import Popup
 
 # Mike's version as of 08/17/24, we'll probably stay around 2.3
 kivy.require('2.3.0')
+
+# ================================================================================================
 
 #* DDS Screen: https://a.co/d/dNE2Vug
 _screen_res = (1024, 600) #desired fix size
 Window.size = _screen_res
 Config.set('graphics', 'resizable', False)
 
-
+# ================================================================================================
 
 class DDSApp(App):
 
     def build(self):
         self.window = GridLayout()
         self.window.cols = 1
-        tm_cherry = "img\SPLASH_screen.png"
+        tm_cherry = "splash\\img\\SPLASH_screen.png"
 
         # car_img = Image(source = "img\SPLASH screen.png")
         # self.window.add_widget(car_img)
@@ -55,25 +61,36 @@ class DDSApp(App):
 
         return self.window
     
-    #* Callback for Button Widget -- part of the main DDSApp() class
-        #! Test code in here, parse later :)
+    #* Callback for Button Widget -- like onClick()
     def callback(self, event):
         print("Button Pushed")
 
-        view = ModalView()
-        view.add_widget(Image(source="img\\fake_qr.jpg"))
-        view.open()
-        
-        
+        # Get size of QR code img, display as pop-up
+            # Kivy Popup: https://kivy.org/doc/stable/api-kivy.uix.popup.html
+        _QRcode = "splash\\img\\fake_qr.jpg"
+        _popup_title_text = "Scan to join the mailing list!"
+        im = cv2.imread(_QRcode)
+        h, w, _ = im.shape
+
+        qr_img = Image(source=_QRcode)
+
+        popup = Popup(title=_popup_title_text,
+                      size_hint=(None, None),
+                      size=(h,w),
+                      content=qr_img
+                      )
+        popup.open()
 
 
 #* Check if screen re-sized, if so reset
-    # Dirty solution from Peter A. (https://stackoverflow.com/questions/37164410/fixed-window-size-for-kivy-programs)
 def reSize(*args):
+   # Dirty solution from Peter A. (https://stackoverflow.com/questions/37164410/fixed-window-size-for-kivy-programs)
    Window.size = _screen_res
    return True    
+
 Window.bind(on_resize = reSize)
 
+# ================================================================================================
 
 #* Run App
 if __name__ == '__main__':
