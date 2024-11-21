86c8984cd31e281c25d54b59e96fc0becdfafce1
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-11-21 15:36:59 -0500

    sendingcan

--- splash/resources/input.py
+++ splash/resources/input.py
@@ -80,7 +80,7 @@ class CANInput(Input):
 
         # this uses the default configuration (for example from the config file)
         # see https://python-can.readthedocs.io/en/stable/configuration.html
-        with self.can_bus as bus:
+        # with self.can_bus as bus:
             # Using specific buses works similar:
             # bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=1000000)
             # bus = can.Bus(interface='pcan', channel='PCAN_USBBUS1', bitrate=250000)
@@ -88,15 +88,15 @@ class CANInput(Input):
             # bus = can.Bus(interface='vector', app_name='CANalyzer', channel=0, bitrate=250000)
             # ...
 
-            msg = can.Message(
-                arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
-            )
+        msg = can.Message(
+            arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
+        )
 
-            try:
-                self.can_bus.send(msg)
-                print(f"Message sent on {bus.channel_info}")
-            except can.CanError:
-                print("Message NOT sent")
+        try:
+            self.can_bus.send(msg)
+            print(f"Message sent on {bus.channel_info}")
+        except can.CanError:
+            print("Message NOT sent")
         
         
     
