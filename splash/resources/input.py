5305df642f1515183b0b007f5e10f2f9260b4c4e
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-11-21 15:30:05 -0500

    sendingcan

--- splash/resources/input.py
+++ splash/resources/input.py
@@ -70,6 +70,33 @@ class CANInput(Input):
     
         for msg in self.can_bus:
             print(self.can_database.decode_message(msg.arbitration_id, msg.data))
+
+
+    
+    def send_can(self):
+        # Code from https://python-can.readthedocs.io/en/stable/
+        """Sends a single message."""
+
+        # this uses the default configuration (for example from the config file)
+        # see https://python-can.readthedocs.io/en/stable/configuration.html
+        with can.Bus() as bus:
+        # Using specific buses works similar:
+        # bus = can.Bus(interface='socketcan', channel='vcan0', bitrate=250000)
+        # bus = can.Bus(interface='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+        # bus = can.Bus(interface='ixxat', channel=0, bitrate=250000)
+        # bus = can.Bus(interface='vector', app_name='CANalyzer', channel=0, bitrate=250000)
+        # ...
+
+            msg = can.Message(
+                arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
+            )
+
+            try:
+                bus.send(msg)
+                print(f"Message sent on {bus.channel_info}")
+            except can.CanError:
+                print("Message NOT sent")
+        
         
     
 
