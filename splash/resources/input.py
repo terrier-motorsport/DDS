cfc6bf6951169b3a14e52c4b892b963aa64d541f
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-11-21 15:32:48 -0500

    sendingcan

--- splash/resources/input.py
+++ splash/resources/input.py
@@ -81,12 +81,12 @@ class CANInput(Input):
         # this uses the default configuration (for example from the config file)
         # see https://python-can.readthedocs.io/en/stable/configuration.html
         with can.Bus() as bus:
-        # Using specific buses works similar:
-        # bus = can.Bus(interface='socketcan', channel='vcan0', bitrate=250000)
-        # bus = can.Bus(interface='pcan', channel='PCAN_USBBUS1', bitrate=250000)
-        # bus = can.Bus(interface='ixxat', channel=0, bitrate=250000)
-        # bus = can.Bus(interface='vector', app_name='CANalyzer', channel=0, bitrate=250000)
-        # ...
+            # Using specific buses works similar:
+            bus = can.Bus(interface='socketcan', channel='can0', bitrate=1000000)
+            # bus = can.Bus(interface='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+            # bus = can.Bus(interface='ixxat', channel=0, bitrate=250000)
+            # bus = can.Bus(interface='vector', app_name='CANalyzer', channel=0, bitrate=250000)
+            # ...
 
             msg = can.Message(
                 arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
