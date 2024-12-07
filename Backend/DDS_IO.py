9937dcd664541dc40e8b0c39cbe1980da90b8af6
Jack Justus <jackjustushome@gmail.com>
2024-12-07 00:20:56 -0500

    delete old input class

--- Backend/DDS_IO.py
+++ Backend/DDS_IO.py
@@ -17,33 +17,18 @@ class DDS_IO:
     # ===== Log File for all sensor data =====
     logFile = File('FullDataLog')
 
+    # ===== Devices that the DDS Talks to =====
     devices = {
         "canInterface" : CANInterface,
-        "digitalAnalogConverter" : ADS1015
+        "coolingLoopSensors" : ADS1015
     }
 
-    # ===== Devices that the DDS Talks to =====
+    # ===== Methods =====
 
     def __init__(self):
-        self.__define_sensors()
+        self.__define_devices()
         pass
 
-    
-    def __define_sensors(self):
-        '''Initializes all sensors for the DDS'''
-
-        # Init CAN interface
-        self.devices['canInterface'] = CANInterface('MC & AMS', can_interface='can0', database_path='Backend/candatabase/CANDatabaseDTI500v2.dbc', logFile=self.logFile)
-        self.devices['canInterface'].add_database('Backend/candatabase/Orion_CANBUSv4.dbc') # Add the DBC file for the AMS to the CAN interface
-
-        # Init i2c bus
-        i2c = smbus.SMBus(1)
-        # Init ADS
-        self.devices['digitalAnalogConverter'] = ADS1015("Cooling loop", File, i2c_bus=i2c)
-
-    def get_data(self, device : Interface, parameter : str):
-        '''Gets a single parameter from a device'''
-        return device.get_data(parameter)
     
     def update(self):
         '''Updates all sensors. Should be called as often as possible.'''
@@ -51,6 +36,36 @@ class DDS_IO:
         # Update all devices
         for device in self.devices:
             device.update()
+
+
+    def get_device(self, deviceKey : str) -> Interface:
+        return self.devices.get(deviceKey)
+
+
+    def __define_devices(self):
+
+        '''Initializes all sensors & interfaces for the DDS'''
+
+        # Init CAN interface & CAN Devices
+        self.devices['canInterface'] = CANInterface('MC & AMS', can_interface='can0', database_path='Backend/candatabase/CANDatabaseDTI500v2.dbc', logFile=self.logFile)
+        self.devices['canInterface'].add_database('Backend/candatabase/Orion_CANBUSv4.dbc') # Add the DBC file for the AMS to the CAN interface
+
+        # Init i2c bus
+        self.i2c_bus = smbus.SMBus(1)
+
+        # Init cooling loop ADS
+        self.devices['coolingLoopSensors'] = ADS1015("Cooling loop", File, i2c_bus=self.i2c_bus)
+        self.devices['coolingLoopSensors'].setChannelNames(
+            'hotPressure',
+            'coldPressure',
+            'hotTemperature',
+            'coldTemperature')
+        self.devices['coolingLoopSensors'].setChannelMinimums(
+            0.5,
+            0.5,
+            0.5,
+            0.5
+        )
 
 
 # Example / Testing Code
@@ -65,16 +80,16 @@ if DEBUG_ENABLED == True:
     while True:
         io.update()
 
-        hotpressure = io.canInterface.get_data("hotPressure")
+        hotpressure = io.get_device('coolingLoopSensors').get_data('hotPressure')
         print(f"hot pressure: {hotpressure}")
 
-        coldpressure = io.canInterface.get_data("coldPressure")
+        coldpressure = io.get_device('coolingLoopSensors').get_data('coldPressure')
         print(f"cold pressure: {coldpressure}")
 
-        hottemp = io.canInterface.get_data("hotTemperature")
+        hottemp = io.get_device('coolingLoopSensors').get_data('hotTemperature')
         print(f"hot temp: {hottemp}")
 
-        coldtemp = io.canInterface.get_data("coldTemperature")
+        coldtemp = io.get_device('coolingLoopSensors').get_data('coldTemperature')
         print(f"cold temp: {coldtemp}")
 
 
