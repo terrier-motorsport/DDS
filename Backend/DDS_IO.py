c7bb04b5218b0ed3fef5c49c9133e46f1596130f
Jack Justus <jackjustushome@gmail.com>
2024-12-10 20:34:19 -0500

    Update DDS_IO.py

--- Backend/DDS_IO.py
+++ Backend/DDS_IO.py
@@ -72,7 +72,9 @@ class DDS_IO:
 
 
             # ===== Init cooling loop inputs & ADS ===== 
-            M3200_value_mapper = ValueMapper(voltage_range=[0.5, 4.5], output_range=[0, 17])
+            M3200_value_mapper = ValueMapper(
+                voltage_range=[0.5, 4.5], 
+                output_range=[0, 17])
 
             # Define constants for NTC_M12 value mapping
             resistance_values = [
@@ -94,10 +96,10 @@ class DDS_IO:
             )
 
             self.devices['coolingLoopSensors'] = ADS_1015("Cooling loop", logger=self.logFile, i2c_bus=self.i2c_bus, inputs = [
-                Analog_In('hotPressure', 'bar', mapper=M3200_value_mapper),           #ADC1(A0)
-                Analog_In('hotTemperature', '°C', mapper=NTC_M12_value_mapper),       #ADC1(A1)
-                Analog_In('coldPressure', 'bar', mapper=M3200_value_mapper),          #ADC1(A2)
-                Analog_In('coldTemperature', '°C', mapper=NTC_M12_value_mapper)       #ADC1(A3)
+                Analog_In('hotPressure', 'bar', mapper=M3200_value_mapper, tolerance=0),           #ADC1(A0)
+                Analog_In('hotTemperature', '°C', mapper=NTC_M12_value_mapper, tolerance=0),       #ADC1(A1)
+                Analog_In('coldPressure', 'bar', mapper=M3200_value_mapper, tolerance=0),          #ADC1(A2)
+                Analog_In('coldTemperature', '°C', mapper=NTC_M12_value_mapper, tolerance=0)       #ADC1(A3)
             ])
             # TODO: Init second ADC w/ other sensors
 
