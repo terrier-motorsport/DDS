8dd810855c03926ee67db56d2a19662e178678ce
Jack Justus <jackjustushome@gmail.com>
2024-12-10 20:35:17 -0500

    Update DDS_IO.py

--- Backend/DDS_IO.py
+++ Backend/DDS_IO.py
@@ -96,10 +96,10 @@ class DDS_IO:
             )
 
             self.devices['coolingLoopSensors'] = ADS_1015("Cooling loop", logger=self.logFile, i2c_bus=self.i2c_bus, inputs = [
-                Analog_In('hotPressure', 'bar', mapper=M3200_value_mapper, tolerance=0),           #ADC1(A0)
-                Analog_In('hotTemperature', '°C', mapper=NTC_M12_value_mapper, tolerance=0),       #ADC1(A1)
-                Analog_In('coldPressure', 'bar', mapper=M3200_value_mapper, tolerance=0),          #ADC1(A2)
-                Analog_In('coldTemperature', '°C', mapper=NTC_M12_value_mapper, tolerance=0)       #ADC1(A3)
+                Analog_In('hotPressure', 'bar', mapper=M3200_value_mapper, tolerance=0.1),           #ADC1(A0)
+                Analog_In('hotTemperature', '°C', mapper=NTC_M12_value_mapper, tolerance=0.1),       #ADC1(A1)
+                Analog_In('coldPressure', 'bar', mapper=M3200_value_mapper, tolerance=0.1),          #ADC1(A2)
+                Analog_In('coldTemperature', '°C', mapper=NTC_M12_value_mapper, tolerance=0.1)       #ADC1(A3)
             ])
             # TODO: Init second ADC w/ other sensors
 
