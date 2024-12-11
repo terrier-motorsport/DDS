cbb9e72f330c13e839df5733d8f9394a52073e9e
Jack Justus <jackjustushome@gmail.com>
2024-12-10 21:12:32 -0500

    Update DDS_IO.py

--- Backend/DDS_IO.py
+++ Backend/DDS_IO.py
@@ -141,3 +141,4 @@ if DEBUG_ENABLED:
             coldtemp = io.get_device('coolingLoopSensors').get_data('coldTemperature')
             print(f"cold temp: {coldtemp}")
             print(io.get_device('coolingLoopSensors').inputs[3].voltage)
+            print(io.get_device('coolingLoopSensors').inputs[3].get_output())
