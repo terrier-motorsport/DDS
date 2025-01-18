52f0fb78812b77f40172753641efa8c408b3b36c
Jack Justus <jackjustushome@gmail.com>
2025-01-18 11:42:26 -0800

    Update dtihv500.py

--- Backend/resources/dtihv500.py
+++ Backend/resources/dtihv500.py
@@ -1,10 +1,10 @@
 # DTI HV 500 (MC) Class for Terrier Motorsport's DDS
     # Code by Jackson Justus (jackjust@bu.edu)
 
-from Backend.device import Device
+from Backend.device import CANDevice
 
 
-class DTI_HV_500(Device):
+class DTI_HV_500(CANDevice):
     '''
     This device contains the logic from the DTI HV 500 Motor Controller
     '''
