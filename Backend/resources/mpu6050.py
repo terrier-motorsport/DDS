08cbc20b4c4022d2cebeff047e4c3fdfafbbce6f
Jack Justus <jackjustushome@gmail.com>
2025-01-24 18:02:36 -0500

    Update mpu6050.py

--- Backend/resources/mpu6050.py
+++ Backend/resources/mpu6050.py
@@ -229,6 +229,9 @@ while True:
 
 	print(f'mpu param names: {mpu.get_all_param_names()}')
 
+	for param_name in mpu.get_all_param_names():
+		print(f"{param_name}: {mpu.get_data(param_name)}")
+
 	time.sleep(0.1)
 	
 
