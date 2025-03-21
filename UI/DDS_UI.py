a344e9719b54243adc4fad10faeab3a4911e516d
annalapradeBU <144203051+annalapradeBU@users.noreply.github.com>
2025-03-21 16:06:34 -0400

    gook9

--- UI/DDS_UI.py
+++ UI/DDS_UI.py
@@ -391,7 +391,7 @@ class Battery (FloatLayout):
         discharge_logo = Image(source='UI/discharge_logo.png', size=(100, 100), size_hint=(None, None), pos=(40, (rect_height/2-120)))
 
 
-        gook1 = Image(source='UI/gook.jpg', size=(200, 200), size_hint=(None, None))
+        gook1 = Image(source='UI/gook.jpg', size=(100, 100), size_hint=(None, None))
         
         # Adds widgets to the battery rectangle 
         self.left_rect.add_widget(battery_label)  
